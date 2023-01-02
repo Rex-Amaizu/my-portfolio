b1f5b3cc5a838e179d011b4482e6aeb68a514026
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2023-01-02 19:30:54 +0100

    setup tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      maxWidth: {
-        container: "107rem", // Adjust the value to your preferred maximum width
+      screens: { sm: "480px", md: "768px", lg: "976px", xl: "1440pd" },
+      animation: {
+        fadeIn: "fadeIn 1.5s",
+        bounce:
+          "bounce 0.5s alternate cubic-bezier(0.95, 0.05, 0.795, 0.035) infinite",
+        slideUp: "slideUp 0.5s",
+        slideUpEaseInOut: "slideUp 0.5s ease-in-out",
+        slideUpCubiBezier: "slideUp 1s cubic-bezier(0.165, 0.84, 0.44, 1)",
       },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      animationDelay: {
+        0: "0s",
+        2: "0.2s",
+        4: "0.4s",
+        6: "0.6s",
       },
-      boxShadow: {
-        "3xl": "0 0 10px rgba(0,0,0,.09)",
-        "4xl": " 0px 4px 4px rgba(0, 0, 0, 0.25)",
+      keyframes: {
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        slideUp: {
+          from: { transform: "translateY(100%)" },
+          to: { transform: "translateY(0)" },
+        },
+        bounce: {
+          from: { transform: "translateY(10px)" },
+          to: { transform: "translateY(0)" },
+        },
       },
     },
   },
   plugins: [],
-};
+}
