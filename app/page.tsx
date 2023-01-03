29e21e6abbc87c18051172793f3687db1165f470
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2023-01-03 19:34:22 +0100

    Imported HeroSection, AboutSection and ProjectSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
-import About from './components/About';
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-
+import AboutSection from "@/components/AboutSection"
+import HeroSection from "@/components/HeroSection"
+import ProjectsSection from "@/components/ProjectsSection"
 export default function Home() {
   return (
-    <main>
-      <Navbar />
-      <Hero />
-      <About />
+    <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
+      <HeroSection />
+      <AboutSection />
+      <ProjectsSection />
     </main>
-  );
+  )
 }
