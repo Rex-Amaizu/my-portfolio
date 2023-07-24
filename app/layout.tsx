696a9c56fcabe9912fb24b79296d4d0ad6654554
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2023-07-24 19:43:20 +0100

    Metadata added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import "../styles/globals.css"
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
 import { ThemeProvider } from "next-themes"
+import type { Metadata } from "next";
+
 
 export default function RootLayout({
   children,
