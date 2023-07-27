e1cbcbb3a693c5bb6194d4559fcbddb74efd5679
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2023-07-27 03:14:52 +0100

    updated head

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,8 +33,6 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👋</text></svg>"
