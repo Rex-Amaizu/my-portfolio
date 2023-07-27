d33d65d99a75ee96a47ee4d93ed7b266849c24f6
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2023-07-27 03:11:56 +0100

    .

--- app/head.tsx
+++ app/head.tsx
@@ -18,7 +18,7 @@ export const metadata = {
 
 export default function Head() {
   return (
-    <>
+    <head>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <title>{metadata.title}</title>
       <meta name="description" content={metadata.description}  />
@@ -26,6 +26,6 @@ export default function Head() {
         rel="icon"
         href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👋</text></svg>"
       />
-    </>
+    </head>
   );
 }
